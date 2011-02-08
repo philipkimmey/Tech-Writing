73d326db1ea15f1bdc0fafac0c04f5615c04065b
Philip Kimmey <philip.kimmey@thinkenzyme.com>
2011-02-08 11:53:16 -0600

    more commits

--- src/techwriting/assignments/models.py
+++ src/techwriting/assignments/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+from techwriting.meetings.models import Meeting
+
+def get_file_path(instance, filename):
+    extension = filename.split('.')[-1]
+    return "%s.%s" % (instance.pk, extension)
+
+class Assignment(models.Model):
+    title = models.CharField(max_length=60)
+    description = models.TextField(blank=True)
+    meetings = models.ManyToManyField(Meeting)
+    
+class Attachment(models.Model):
+    title = models.CharField(max_length=60)
+    meetings = models.ManyToManyField(Meeting, blank=True)
+    assignments = models.ManyToManyField(Assignment, blank=True)
+    file = models.FileField(upload_to=get_file_path, max_length=200)
+    def __unicode__(self):
+        return self.title
--- src/techwriting/meetings/models.py
+++ src/techwriting/meetings/models.py
@@ -1,19 +1,22 @@
 from django.db import models
 
+from django.contrib.auth.models import User
+
 # Create your models here.
 class Meeting(models.Model):
-    title = models.CharField(max_length=50)
     start = models.DateTimeField()
     finish = models.DateTimeField()
-    short_description = models.CharField(max_length=60)
-    description = models.TextField()
 
 class ClassMeeting(Meeting):
+    title = models.CharField(max_length=50)
+    short_description = models.CharField(max_length=60)
+    description = models.TextField()
     def __unicode__(self):
         return self.title
     def get_absolute_url(self):
         return "/course/%s/" % self.pk
 
 class GradingMeeting(Meeting):
+    user = models.ForeignKey(User, null=True, blank=True)
     def __unicode__(self):
         return self.title
